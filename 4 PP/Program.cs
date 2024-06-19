31069c9cb2b310225caef24c5999fb70fb2fc990
z_moff <zmoffzema@gmail.com>
2024-06-20 00:27:27 +0300

    Добавьте файлы проекта.

---
+++ 4 PP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+class Countdown
+{
+    static void Main()
+    {
+        Console.WriteLine("Обратный отсчет");
+
+        // Запрашиваем количество секунд у пользователя
+        Console.Write("Введите количество секунд: ");
+        int seconds;
+        if (int.TryParse(Console.ReadLine(), out seconds) && seconds > 0)
+        {
+            // Выполняем обратный отсчет
+            for (int i = seconds; i >= 0; i--)
+            {
+                Console.Clear();
+                Console.WriteLine($"Осталось {i} секунд(ы)");
+                Thread.Sleep(1000); // Задержка на 1 секунду
+            }
+
+            Console.WriteLine("Обратный отсчет завершен!");
+        }
+        else
+        {
+            Console.WriteLine("Пожалуйста, введите корректное количество секунд.");
+        }
+    }
+}
